78a4cce10bee33b7ce3937ffc468a6c14d835d02
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-12 14:08:18 +0700

    IIOT-14093: add mssql connection

---
+++ kumeza/integration/source/mssql/mssql.py
@@ -0,0 +1,21 @@
+from kumeza.config.ingestor_config import IngestionConfig
+
+class MSSQL:
+
+    def __init__(self):
+        self.authentication_type = None
+        self.database_instance = None
+        self.hostname = None
+        self.username = None
+        self.password = None
+        self.port = None
+        self.driver = None
+
+    def build_connection(self, config: IngestionConfig):
+        self.authentication_type = config.source_system.authentication_type
+        self.database_instance = config.source_system.database_instance
+        self.hostname = config.source_system.hostname
+        self.username = config.credentials.username
+        self.password = config.credentials or "placeholder"
+        self.port = config.source_system.port
+        self.driver = config.integration.driver or 'ODBC Driver 18 for SQL Server'
