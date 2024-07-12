2eed5f91b2f8b9c09eb3824500f5958d8de15549
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-12 15:18:40 +0700

    IIOT-14093: load environment variables

--- kumeza/integration/source/mssql/mssql.py
+++ kumeza/integration/source/mssql/mssql.py
@@ -1,8 +1,15 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 
 from kumeza.config.ingestor_config import IngestionConfig
 
-@pytest.mark.skip(reason="no way of currently testing this")
+
+# Load environment variables
+load_dotenv()
+
+
 class MSSQL:
     def __init__(self):
         self.authentication_type = None
@@ -13,12 +20,11 @@ class MSSQL:
         self.port = None
         self.driver = None
 
-    
     def build_connection(self, config: IngestionConfig):
         self.authentication_type = config.source_system.authentication_type
         self.database_instance = config.source_system.database_instance
         self.hostname = config.source_system.hostname
-        self.username = config.credentials.username
-        self.password = config.credentials or "placeholder"
+        self.username = config.credentials.username or os.getenv("DB_USERNAME")
+        self.password = config.credentials or os.getenv("DB_PASSWORD")
         self.port = config.source_system.port
         self.driver = config.integration.driver or "ODBC Driver 18 for SQL Server"
