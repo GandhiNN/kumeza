aac4a2da1faa121334bb373fd33762c5c5a3ee45
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-15 18:49:57 +0700

    IIOT-14093: test for SecretsManager

---
+++ tests/utils/aws/secretsmanager/test_secrets_manager.py
@@ -0,0 +1,40 @@
+import boto3
+import pytest
+from moto import mock_aws
+
+from kumeza.utils.aws.secretsmanager.secretsmanager import SecretsManager
+
+
+@pytest.fixture
+def create_mocked_secret_manager_connection():
+    with mock_aws():
+        yield boto3.session.Session().client(
+            service_name="secretsmanager", region_name="eu-west-1"
+        )
+
+
+@pytest.fixture
+def create_test_secret(create_mocked_secret_manager_connection):
+    create_mocked_secret_manager_connection.create_secret(
+        Name="mock_secret",
+        SecretString="""{"mock_secret_key": "mock_secret_value"}""",
+    )
+
+
+def test_retrieve_secret_string(
+    monkeypatch, create_mocked_secret_manager_connection, create_test_secret
+):
+    def get_mocked_secret_manager(*args, **kwargs):
+        return create_mocked_secret_manager_connection
+
+    monkeypatch.setattr(
+        SecretsManager,
+        "_create_boto_client",
+        get_mocked_secret_manager,
+    )
+
+    credentials_client = SecretsManager("mock_secret")
+
+    assert credentials_client.get_secret_value() == {
+        "mock_secret_key": "mock_secret_value"
+    }
