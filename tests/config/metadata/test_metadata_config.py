79f22483dbcc4e02fe9544befc2c9673a896765a
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-15 14:37:29 +0700

    IIOT-14093: metadata test class

--- tests/config/metadata/test_metadata_config.py
+++ tests/config/metadata/test_metadata_config.py
@@ -11,15 +11,7 @@ JSON_CONFIG = os.path.join(ABS_PATH, "..", "files", "config.json")
 YAML_CONFIG = os.path.join(ABS_PATH, "..", "files", "config.yaml")
 
 
-class TestConfigInstanceMetadata:
-    def __init__(self):
-        self.metadata = MetadataConfig(
-            sink_type="dynamodb",
-            table_name="daas-imel-ingestion-status-dev",
-        )
-
-
-class TestSetUp:  # pragma: no cover
+class SetUp:
     def __init__(self):
         self.json_config = None
         self.yml_config = None
@@ -29,31 +21,55 @@ class TestSetUp:  # pragma: no cover
         self.yml_config = ConfigLoader.load(YAML_CONFIG)
 
 
-class TestMetadataConfig(unittest.TestCase, TestSetUp):
+class ConfigInstanceMetadata:
+    def __init__(self):
+        # self.metadata = MetadataConfig(
+        #     sink_type="dynamodb",
+        #     table_name="daas-imel-ingestion-status-dev",
+        # )
+        self.metadata_yaml = ConfigLoader.load(YAML_CONFIG)["metadata"]
+        self.metadata_json = ConfigLoader.load(JSON_CONFIG)["metadata"]
+
+
+class MetadataConfigTest(unittest.TestCase, SetUp):
     def setUp(self) -> None:
         self.setup()
 
     def test(self):
-        base_config = TestConfigInstanceMetadata()
-        expected = base_config.metadata
+        base_config = ConfigInstanceMetadata()
+        # expected = base_config.metadata
+        keys_yaml = list(base_config.metadata_yaml.keys())
+        keys_json = list(base_config.metadata_json.keys())
+
+        # Keys sameness assertion
         self.assertEqual(
-            MetadataConfig.marshal(self.yml_config["metadata"]),
-            expected,
+            MetadataConfig.marshal(self.yml_config["metadata"]).get_field_name(),
+            keys_yaml,
         )
         self.assertEqual(
-            MetadataConfig.marshal(self.json_config["metadata"]),
-            expected,
+            MetadataConfig.marshal(self.json_config["metadata"]).get_field_name(),
+            keys_json,
+        )
+
+        # Object length assertion
+        self.assertEqual(
+            MetadataConfig.marshal(self.yml_config["metadata"]).get_length(),
+            len(keys_yaml),
+        )
+        self.assertEqual(
+            MetadataConfig.marshal(self.json_config["metadata"]).get_length(),
+            len(keys_json),
         )
 
 
-def testSuite():  # pragma: no cover
+def testSuite():
     suite = unittest.TestSuite()
     suite.addTests(
         unittest.TestLoader().loadTestsFromTestCase(
-            TestMetadataConfig,
+            MetadataConfigTest,
         )
-    )  # pragma: no cover
+    )
 
 
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(testSuite())  # pragma: no cover
+    unittest.TextTestRunner(verbosity=2).run(testSuite())
