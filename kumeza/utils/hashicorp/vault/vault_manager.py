679031e1dfe3f605871ea412efc53da5d9641ead
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-19 18:46:36 +0700

    IIOT-14093: init class

--- kumeza/utils/hashicorp/vault/vault_manager.py
+++ kumeza/utils/hashicorp/vault/vault_manager.py
@@ -6,13 +6,14 @@ from kumeza.config.ingestor_config import IngestionConfig
 class VaultManager:
 
     def __init__(self, ingestion_config=IngestionConfig):
-        self.namespace: str = ingestion_config.credentials.namespace
-        self.env: str = ingestion_config.runtime_environment.env
-        self.vault_url: str = ingestion_config.credentials.url
-        self.verify_ssl: bool = ingestion_config.credentials.verify_ssl
-        self.client: hvac.Client = None
-
-    def setup_client(self):
-        self.client = hvac.Client(
-            url=self.vault_url, namespace=self.namespace, verify=self.verify_ssl
+        self.client: hvac.Client = hvac.Client(
+            url=ingestion_config.credentials.url,
+            namespace=ingestion_config.credentials.namespace,
+            verify=ingestion_config.credentials.verify_ssl,
         )
+
+    def approle_login(self, role_id: str, secret_id: str):
+        self.client.auth.approle.login(role_id=role_id, secret_id=secret_id)
+
+    def get_secret(self):
+        pass
