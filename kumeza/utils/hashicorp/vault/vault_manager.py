f276300bf93c9159129f66f548e61ce41944296d
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-23 11:36:46 +0700

    IIOT-14093: make modules visible

--- kumeza/utils/hashicorp/vault/vault_manager.py
+++ kumeza/utils/hashicorp/vault/vault_manager.py
@@ -5,16 +5,18 @@ from kumeza.config.ingestor_config import IngestionConfig
 
 class VaultManager:
 
-    def __init__(self, ingestion_config=IngestionConfig):
+    def __init__(self):
         self.client = None
 
     def create_vault_client(self, config: dict):
         self.client = hvac.Client(
-            url=config.url, namespace=config.namespace, verify=config.verify_ssl
+            url=config['url'], namespace=config['namespace'], verify=config['verify_ssl']
         )
 
     def approle_login(self, role_id: str, secret_id: str):
         self.client.auth.approle.login(role_id=role_id, secret_id=secret_id)
 
-    def get_secret(self):
-        pass
+    def get_secret(self, path: str, mount_point: str) -> dict:
+        return self.client.secrets.kv.v1.read_secret(
+            path=path, mount_point=mount_point
+        )
