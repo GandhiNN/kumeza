1a9a035e13f5a012cb611765edd84e85f9b00c62
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-24 13:18:44 +0700

    IIOT-14093: refactor using unittest [skip ci]

--- tests/utils/aws/secretsmanager/test_secrets_manager.py
+++ tests/utils/aws/secretsmanager/test_secrets_manager.py
@@ -1,7 +1,8 @@
 # pylint: disable=redefined-outer-name
 #
+import unittest
+
 import boto3
-import pytest
 from moto import mock_aws
 
 from kumeza.utils.aws.secretsmanager.secretsmanager import SecretsManager
@@ -10,34 +11,29 @@ from kumeza.utils.aws.secretsmanager.secretsmanager import SecretsManager
 SECRET_NAME = "mock_secret"
 
 
-@pytest.fixture
-def create_mocked_secret_manager_connection():
-    with mock_aws():
-        yield boto3.session.Session().client(
+class SecretsManagerTestIntegration(unittest.TestCase):
+
+    @mock_aws
+    def setUp(self):
+        # Setup the mock connection
+        self.secretsmanager_client = boto3.session.Session().client(
             service_name="secretsmanager", region_name="eu-west-1"
         )
 
+    @mock_aws
+    def create_test_secret(self):
+        self.secretsmanager_client.create_secret(
+            Name="mock_secret",
+            SecretString="""{"mock_secret_key": "mock_secret_value"}""",
+        )
 
-@pytest.fixture
-def create_test_secret(create_mocked_secret_manager_connection):
-    create_mocked_secret_manager_connection.create_secret(
-        Name="mock_secret",
-        SecretString="""{"mock_secret_key": "mock_secret_value"}""",
-    )
+    @mock_aws
+    def test_retrieve_secret_string(self):
+        # Create the mock secret
+        self.create_test_secret()
 
-
-def test_retrieve_secret_string(
-    monkeypatch, create_mocked_secret_manager_connection, create_test_secret
-):
-    def get_mocked_secret_manager_connection(*args, **kwargs):
-        return create_mocked_secret_manager_connection
-
-    monkeypatch.setattr(
-        SecretsManager, "_create_boto_client", get_mocked_secret_manager_connection
-    )
-
-    credentials_client = SecretsManager()
-
-    assert credentials_client.get_secret(SECRET_NAME) == {
-        "mock_secret_key": "mock_secret_value"
-    }
+        # Run the test for the function
+        credentials_client = SecretsManager()
+        assert credentials_client.get_secret(SECRET_NAME) == {
+            "mock_secret_key": "mock_secret_value"
+        }
