981e689eee7f75a92abebd1114cef34af096c650
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-25 11:24:45 +0700

    IIOT-14093: init test_jdbc [skip ci]

---
+++ tests/connectors/test_jdbc.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class JDBCTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_connstring(self):
+        pass
+
+    def test_get_driver(self):
+        pass
