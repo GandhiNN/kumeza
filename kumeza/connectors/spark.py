aff9ad59da8e6408f0da5d0d4729748b28b6e068
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-26 15:45:38 +0700

    IIOT-14093: init spark class and unittest

--- kumeza/connectors/spark.py
+++ kumeza/connectors/spark.py
@@ -1,51 +1,16 @@
-class JDBCManager:
+from pyspark.sql import SparkSession
 
-    def __init__(
-        self, hostname: str, port: str, db_instance: str, db_name: str, domain: str
-    ):
-        self.hostname = hostname
-        self.port = port
-        self.db_instance = db_instance
-        self.db_name = db_name
-        self.domain = domain
+class SparkManager:
+    
+    def __init__(self):
+        self.session = None
 
-    def get_connection_string(self, db_type: str) -> str:
-        if db_type == "mssql":
-            return (
-                """jdbc:jtds:sqlserver://"""
-                f"""{self.hostname}:{self.port};instance={self.db_instance};"""
-                f"""databaseName={self.db_name};integratedSecurity=false"""
-            )
-        if db_type == "mssql-ntlm":
-            return (
-                """jdbc:jtds:sqlserver://"""
-                f"""{self.hostname}:{self.port};instance={self.db_instance};"""
-                f"""databaseName={self.db_name};integratedSecurity=true;"""
-                f"""useNTLMv2=true;domain={self.domain}"""
-            )
-        if db_type == "postgresql":
-            return (
-                """jdbc:jtds:sqlserver://"""
-                f"""{self.hostname}:{self.port};instance={self.db_instance};"""
-                f"""databaseName={self.db_name};integratedSecurity=true;"""
-                f"""useNTLMv2=true;domain={self.domain}"""
-            )
-        if db_type == "oracle":
-            return (
-                f"""jdbc:oracle:thin:@{self.hostname}:{self.port}:{self.db_instance}"""
-            )
-        if db_type == "mysql":
-            return (
-                f"""jdbc:mysql://{self.hostname}:{self.port}/{self.db_name}"""
-                """?zeroDateTimeBehavior=CONVERT_TO_NULL&autoCommit=false"""
-            )
-        return ""
+    def start_session(self, pipeline_name: str):
+        self.session = (
+            SparkSession.builder.config("spark.logConf", "true")
+            .appName(pipeline_name)
+            .getOrCreate()
+        )
 
-    def get_driver(self, db_type: str) -> str:
-        if db_type == "postgresql" or "mssql" in db_type:
-            return "net.sourceforge.jtds.jdbc.Driver"
-        if db_type == "oracle":
-            return "oracle.jdbc.driver.OracleDriver"
-        if db_type == "mysql":
-            return "com.mysql.cj.jdbc.Driver"
-        return ""
+    def stop_session(self):
+        self.session.stop()
--- tests/connectors/test_spark.py
+++ tests/connectors/test_spark.py
@@ -1,88 +1,25 @@
 import unittest
 
-from kumeza.connectors.spark import JDBCManager
+from pyspark.sql import SparkSession
+
+from kumeza.connectors.spark import SparkManager
 
 
-# Constant definition
-HOSTNAME = "somedbhostname.db.sdi.pmi"
-PORT = "1443"
-DB_INSTANCE = "dev"
-DB_NAME = "somedbname"
-DOMAIN = "somedomain.net"
+PIPELINE_NAME = "test_pipeline"
 
 
-class JDBCManagerTest(unittest.TestCase):
+class SparkManagerTest(unittest.TestCase):
 
-    def setUp(self):
-        self.hostname = HOSTNAME
-        self.port = PORT
-        self.db_instance = DB_INSTANCE
-        self.db_name = DB_NAME
-        self.domain = DOMAIN
-        self.jdbc_manager = JDBCManager(
-            self.hostname, self.port, self.db_instance, self.db_name, self.domain
-        )
+    @classmethod
+    def setUpClass(cls):
+        cls.spark_manager = SparkManager()
+        cls.spark_manager.start_session(PIPELINE_NAME)
 
-    def test_get_connstring_mssql(self):
-        assert (
-            self.jdbc_manager.get_connection_string("mssql")
-            == """jdbc:jtds:sqlserver://somedbhostname.db.sdi.pmi:1443;"""
-            """instance=dev;databaseName=somedbname;"""
-            """integratedSecurity=false"""
-        )
-        assert (
-            self.jdbc_manager.get_connection_string("mssql-ntlm")
-            == """jdbc:jtds:sqlserver://somedbhostname.db.sdi.pmi:1443;"""
-            """instance=dev;databaseName=somedbname;"""
-            """integratedSecurity=true;useNTLMv2=true;domain=somedomain.net"""
-        )
+    def test_start_spark_session(self):
+        assert isinstance(self.spark_manager.session, SparkSession)
 
-    def test_get_connstring_postgresql(self):
-        assert (
-            self.jdbc_manager.get_connection_string("postgresql")
-            == """jdbc:jtds:sqlserver://somedbhostname.db.sdi.pmi:1443;"""
-            """instance=dev;databaseName=somedbname;"""
-            """integratedSecurity=true;useNTLMv2=true;domain=somedomain.net"""
-        )
-
-    def test_get_connstring_oracle(self):
-        assert (
-            self.jdbc_manager.get_connection_string("oracle")
-            == """jdbc:oracle:thin:@somedbhostname.db.sdi.pmi:1443:dev"""
-        )
-
-    def test_get_connstring_mysql(self):
-        assert (
-            self.jdbc_manager.get_connection_string("mysql")
-            == """jdbc:mysql://somedbhostname.db.sdi.pmi:1443/somedbname"""
-            """?zeroDateTimeBehavior=CONVERT_TO_NULL&autoCommit=false"""
-        )
-
-    def test_get_connstring_unrecognized_input(self):
-        assert self.jdbc_manager.get_connection_string("unrecognized") == ""
-
-    def test_get_driver_mssql(self):
-        assert (
-            self.jdbc_manager.get_driver("mssql") == "net.sourceforge.jtds.jdbc.Driver"
-        )
-        assert (
-            self.jdbc_manager.get_driver("mssql-ntlm")
-            == "net.sourceforge.jtds.jdbc.Driver"
-        )
-
-    def test_get_driver_postgresql(self):
-        assert (
-            self.jdbc_manager.get_driver("postgresql")
-            == "net.sourceforge.jtds.jdbc.Driver"
-        )
-
-    def test_get_driver_oracle(self):
-        assert (
-            self.jdbc_manager.get_driver("oracle") == "oracle.jdbc.driver.OracleDriver"
-        )
-
-    def test_get_driver_mysql(self):
-        assert self.jdbc_manager.get_driver("mysql") == "com.mysql.cj.jdbc.Driver"
-
-    def test_get_driver_unrecognized_input(self):
-        assert self.jdbc_manager.get_driver("unrecognized") == ""
+    def test_stop_spark_session(self):
+        self.spark_manager.stop_session()
+        NoneType = type(None)
+        print(type(self.spark_manager.session))
+        assert isinstance(self.spark_manager.session._sc._jsc, NoneType)
