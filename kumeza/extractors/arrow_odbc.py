2e6dc14fd9333da2ab46d9563ff6fc2876324a8b
Ngakan Gandhi <ngandhi@pmintl.net>
2024-07-30 14:58:13 +0700

    IIOT-14093: commit arrow odbc class

---
+++ kumeza/extractors/arrow_odbc.py
@@ -0,0 +1,27 @@
+import pyarrow
+from arrow_odbc import read_arrow_batches_from_odbc
+
+
+class ArrowODBC:
+
+    def __init__(self, username: str, password: str, batch_size: int):
+        self.username = username
+        self.password = password
+        self.batch_size = batch_size
+
+    def read(
+        self,
+        query: str,
+        odbc_connstring: str,
+        concurrent: bool = False,
+    ) -> pyarrow.RecordBatch:
+        reader = read_arrow_batches_from_odbc(
+            query=query,
+            connection_string=odbc_connstring,
+            batch_size=self.batch_size,
+            user=self.username,
+            password=self.password,
+        )
+        if concurrent:
+            reader.fetch_concurrently()
+        return reader
