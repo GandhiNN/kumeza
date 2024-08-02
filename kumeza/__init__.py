fefa9b422141a1004776aa589221314cdcd363c0
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-02 11:31:24 +0700

    IIOT-14093: use pkg_name

--- kumeza/__init__.py
+++ kumeza/__init__.py
@@ -1,8 +1,21 @@
-import pathlib
+import importlib.resources as resources
+
+# import pathlib
 from enum import Enum
 
+from kumeza import __name__ as pkg_name
 
-PARENT = pathlib.Path(__file__).parent
+
+# PARENT = pathlib.Path(__file__).parent
+PARENT = resources.files(pkg_name)
+
+# class StaticFiles(Enum):
+#     PKI_CERT = PARENT / "shared_lib/cert/pki_bundle.crt"
+#     JTDS_JAR = PARENT / "shared_lib/jars/jtds-1.3.1.jar"
+#     ORACLE_JAR = PARENT / "shared_lib/jars/ojdbc8.jar"
+#     MYSQL_JAR = PARENT / "shared_lib/jars/mysql-connector-java-8.0.31.jar"
+#     ODBC_CONFIG = PARENT / "shared_lib/odbc/odbcinst.ini"
+#     ODBC_TDS_EXTENSION = PARENT / "shared_lib/odbc/extensions/libtdsodbc.so"
 
 
 class StaticFiles(Enum):
