52436ddd44954544a9511e12eaafc25df79bf88d
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-07 10:56:15 +0700

    IIOT-14093: add string sanitizers module

---
+++ kumeza/utils/stringop/sanitizers.py
@@ -0,0 +1,20 @@
+import re
+
+
+class Sanitizers:
+
+    @staticmethod
+    def replace_multiple_whitespace_with_one_whitespace(s: str) -> str:
+        return re.sub(r"\s+", " ", s)
+
+    @staticmethod
+    def remove_leading_and_trailing_whitespace(s: str) -> str:
+        return re.sub(r"^\s+|\s+$", "", s)
+
+    @staticmethod
+    def remove_leading_and_trailing_backticks(s: str) -> str:
+        return re.sub(r"^`+|`+$", "", s)
+
+    @staticmethod
+    def replace_special_characters_with_underscore(s: str) -> str:
+        return re.sub(r"[ <>/!?+@()%#\"\\]", "_", s)
