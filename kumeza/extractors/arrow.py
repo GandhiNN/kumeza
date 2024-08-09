464a0432e8b0b7486af0d83c69a899d2af9fb864
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-09 12:11:49 +0700

    IIOT-14093: rename module

---
+++ kumeza/extractors/arrow.py
@@ -0,0 +1,38 @@
+import pyarrow
+from arrow_odbc import read_arrow_batches_from_odbc
+
+from kumeza.connectors.odbc import ODBCManager
+
+
+class ArrowExtractor:
+
+    def __init__(self, odbcmanager: ODBCManager):
+        self.odbcmanager = odbcmanager
+
+    def read(
+        self,
+        db_engine: str,
+        db_name: str,
+        sqlquery: str,
+        username: str,
+        password: str,
+        uid: str,
+        fetchsize: int = 1000000,
+        concurrent: bool = False,
+    ) -> pyarrow.RecordBatch:
+        match db_engine:
+            case "mssql" | "mssql-ntlm":
+                reader = read_arrow_batches_from_odbc(
+                    query=sqlquery,
+                    connection_string=self.odbcmanager.get_connection_string(
+                        db_engine, db_name, uid, username, password
+                    ),
+                    batch_size=fetchsize,
+                    user=username,
+                    password=password,
+                )
+                if concurrent:
+                    reader.fetch_concurrently()
+                return reader
+            case _:
+                raise ValueError(f"{db_engine}: Database Engine is not implemented!")
