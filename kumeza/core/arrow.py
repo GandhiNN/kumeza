a01007fc024b34ee5e7ed3e6e727c273ceedf057
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-15 14:55:39 +0700

    IIOT-14093: reformat return value of tds extractor

--- kumeza/core/arrow.py
+++ kumeza/core/arrow.py
@@ -1,2 +1,9 @@
-class ArrowManager:
-    pass
+from typing import Any
+
+import pyarrow as pa
+
+
+class ArrowConverter:
+
+    def from_python_list(self, result_sets: list[tuple[Any, ...]]) -> pa.Table:
+        pass
--- kumeza/extractors/tds.py
+++ kumeza/extractors/tds.py
@@ -1,9 +1,30 @@
-from typing import Any, Union
+# from typing import Any, Union
 
 import pymssql
 
 
 class TDSExtractor:
+
+    # def read(
+    #     self,
+    #     hostname: str,
+    #     port: str,
+    #     db_name: str,
+    #     db_instance: str,
+    #     sqlquery: str,
+    #     domain: str,
+    #     username: str,
+    #     password: str,
+    # ) -> Union[list[tuple[Any, ...]], None]:
+    #     conn = pymssql.connect(
+    #         server=f"{hostname}:{port}\\{db_instance}",
+    #         user=f"{domain}\\{username}",
+    #         password=password,
+    #         database=db_name,
+    #     )
+    #     cursor = conn.cursor()
+    #     cursor.execute(sqlquery)
+    #     return cursor.fetchall()
 
     def read(
         self,
@@ -15,7 +36,7 @@ class TDSExtractor:
         domain: str,
         username: str,
         password: str,
-    ) -> Union[list[tuple[Any, ...]], None]:
+    ):
         conn = pymssql.connect(
             server=f"{hostname}:{port}\\{db_instance}",
             user=f"{domain}\\{username}",
@@ -24,4 +45,17 @@ class TDSExtractor:
         )
         cursor = conn.cursor()
         cursor.execute(sqlquery)
-        return cursor.fetchall()
+
+        def _return_dict_pair(row_item):
+            return_dict = {}
+            for column_name, row in zip(cursor.description, row_item):
+                return_dict[column_name[0]] = row
+            return return_dict
+
+        return_list = []
+        for row in cursor:
+            row_item = _return_dict_pair(row)
+            return_list.append(row_item)
+
+        conn.close()
+        return return_list
