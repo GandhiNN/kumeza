8274bb84c0f4f48b9c167129fe1d1e60bbed7c4b
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-15 15:22:56 +0700

    IIOT-14093: add converter method

--- kumeza/core/arrow.py
+++ kumeza/core/arrow.py
@@ -5,5 +5,5 @@ import pyarrow as pa
 
 class ArrowConverter:
 
-    def from_python_list(self, result_sets: list[tuple[Any, ...]]) -> pa.Table:
-        pass
+    def from_python_list(self, result_sets: list[dict[str, Any]]) -> pa.Table:
+        return pa.Table.from_pylist(result_sets)
