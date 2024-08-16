3b0ab94941a4fb5b68f7c6c248cc8e7703fa90f5
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-16 11:55:14 +0700

    IIOT-14093: add ArrowManager

--- kumeza/core/arrow.py
+++ kumeza/core/arrow.py
@@ -7,3 +7,8 @@ class ArrowConverter:
 
     def from_python_list(self, result_sets: list[dict[str, Any]]) -> pa.Table:
         return pa.Table.from_pylist(result_sets)
+
+class ArrowManager:
+
+    def get_schema(self, table: pa.Table):
+        return table.schema
