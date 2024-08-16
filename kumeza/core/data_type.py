499d698f10753231a443db479a1a15cc2856405a
Ngakan Gandhi <ngandhi@pmintl.net>
2024-08-16 16:30:18 +0700

    IIOT-14093: arrow to hive transform_schema

--- kumeza/core/data_type.py
+++ kumeza/core/data_type.py
@@ -16,5 +16,23 @@ class SparkToHiveMapping:
             "DecimalType": "double",
         }
 
-    def transform_schema(self, spark_schema: dict):
+    def transform_schema(self, spark_dtype: str):
         pass
+
+
+class ArrowToHiveMapping:
+
+    def __init__(self):
+        self.dtype_mapping = {
+            "int64": "int",
+            "timestamp[us]": "string",
+            "string": "string",
+            "bool": "string",
+            "null": "string",
+        }
+
+    def transform_schema(self, arrow_dtype: str):
+        try:
+            return self.dtype_mapping[arrow_dtype]
+        except Exception as e:
+            raise ValueError(f"{arrow_dtype} is not implemented yet") from e
