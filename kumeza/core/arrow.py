fe9ebb185204473f5bb2307fb59ae6d02568dc32
Ngakan Gandhi <ngandhi@pmintl.net>
2024-09-02 16:49:45 +0700

    IIOT-14093: scaffold delta_lake reader

--- kumeza/core/arrow.py
+++ kumeza/core/arrow.py
@@ -3,17 +3,18 @@ from typing import Any
 
 import pyarrow as pa
 import pyarrow.parquet as pq
+import s3fs
 
 from kumeza.core.data import ArrowToHiveMapping
 
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class ArrowConverter:
 
     def from_python_list(self, result_sets: list[dict[str, Any]]) -> pa.Table:
-        log.info("Converting input to PyArrow table")
+        logger.info("Converting input to PyArrow table")
         return pa.Table.from_pylist(result_sets)
 
 
@@ -22,7 +23,7 @@ class ArrowManager:  # pragma: no cover
     @classmethod
     def get_schema(cls, table: pa.Table, hive: bool = False) -> list[dict]:
         schema = []
-        log.info("Getting table schema from Arrow table. Hive mapping = %s", hive)
+        logger.info("Getting table schema from Arrow table. Hive mapping = %s", hive)
         for field in table.schema:
             s = {
                 "name": str(field.name),
@@ -39,5 +40,11 @@ class ArrowManager:  # pragma: no cover
 
     @classmethod
     def write_to_s3(cls, table: pa.Table, s3uri: str):
-        log.info("Writing Arrow table to %s", s3uri)
+        logger.info("Writing Arrow table to %s", s3uri)
         pq.write_to_dataset(table, root_path=s3uri)
+
+    @classmethod
+    def read_from_s3(cls, s3uri: str) -> pq.ParquetDataset:
+        logger.info("Reading parquet file into PyArrow table")
+        fs = s3fs.S3FileSystem(anon=False, use_ssl=True)
+        return pq.ParquetDataset(s3uri, filesystem=fs)
