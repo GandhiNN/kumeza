874be4aeeb9203c7f6cda3ffb7a0be0434151b0a
Ngakan Gandhi <ngandhi@pmintl.net>
2024-09-02 13:24:27 +0700

    IIOT-14093: reuse connection pool

--- kumeza/extractors/mssql.py
+++ kumeza/extractors/mssql.py
@@ -11,8 +11,21 @@ logger = logging.getLogger(__name__)
 
 class MSSQLExtractor:
 
-    def __init__(self, tdsmanager: TDSManager):
+    def __init__(
+        self,
+        tdsmanager: TDSManager,
+        domain: str,
+        username: str,
+        password: str,
+        db_name: str,
+    ):
         self.tdsmanager = tdsmanager
+        self.conn = pymssql.connect(
+            server=self.tdsmanager.get_connection_string(),
+            user=f"{domain}\\{username}",
+            password=password,
+            database=db_name,
+        )
 
     def _return_dict_pair(
         self, cursor: pymssql.Cursor, row_item: list[Tuple[str, Any]]
@@ -22,23 +35,22 @@ class MSSQLExtractor:
             return_dict[column_name[0]] = row
         return return_dict
 
+    def close_connection(self):
+        self.conn.close()
+
     def read(
         self,
-        db_name: str,
         sqlquery: str,
-        domain: str,
-        username: str,
-        password: str,
     ) -> list[dict[str, Any]]:
         try:
             logger.info("Connecting to the database")
-            conn = pymssql.connect(
-                server=self.tdsmanager.get_connection_string(),
-                user=f"{domain}\\{username}",
-                password=password,
-                database=db_name,
-            )
-            cursor = conn.cursor()
+            # conn = pymssql.connect(
+            #     server=self.tdsmanager.get_connection_string(),
+            #     user=f"{domain}\\{username}",
+            #     password=password,
+            #     database=db_name,
+            # )
+            cursor = self.conn.cursor()
             cursor.execute(sqlquery)
 
             return_list = []
@@ -46,7 +58,6 @@ class MSSQLExtractor:
                 row_item = self._return_dict_pair(cursor, row)
                 return_list.append(row_item)
 
-            conn.close()
             return return_list
         except Exception as e:
             logger.error(e)
