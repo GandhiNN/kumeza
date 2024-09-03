6e430fbfb7055bd9b4deeb61757e60073c63c804
Ngakan Gandhi <ngandhi@pmintl.net>
2024-09-03 14:09:28 +0700

    IIOT-14093: refactor arguments

--- kumeza/extractors/wrappers/mssql_tds/sequential.py
+++ kumeza/extractors/wrappers/mssql_tds/sequential.py
@@ -8,10 +8,16 @@ logger = logging.getLogger(__name__)
 
 class MSSQLSequentialRunner(MSSQLRunner):
 
-    def execute(self, task: str, ingestion_objects: list[dict]):
+    def execute(
+        self,
+        task: str,
+        ingestion_objects: list[dict],
+        schema_sink_id: str,
+        raw_sink_id: str,
+    ):
         if task == "schema":
             for t_object in ingestion_objects:
-                self.ingest_schema(t_object)
+                self.ingest_schema(t_object, schema_sink_id)
         if task == "raw":
             for t_object in ingestion_objects:
-                self.ingest_raw(t_object)
+                self.ingest_raw(t_object, raw_sink_id)
