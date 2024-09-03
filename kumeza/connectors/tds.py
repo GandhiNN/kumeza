bc2d5693608f0c654a2463be3a9dd855a2c0b61e
Ngakan Gandhi <ngandhi@pmintl.net>
2024-09-03 10:37:23 +0700

    IIOT-14093: fix type hinting for port

--- kumeza/connectors/tds.py
+++ kumeza/connectors/tds.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 class TDSManager:
 
-    def __init__(self, hostname: str, port: str, db_instance: str):
+    def __init__(self, hostname: str, port: int, db_instance: str):
         self.hostname = hostname
         self.port = port
         self.db_instance = db_instance
