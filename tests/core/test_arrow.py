2a61bc7eda428a1fb211a2de99daf5207dea8b45
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-18 14:14:47 +0700

    IIOT-14913: test arrow from_csv

--- tests/core/test_arrow.py
+++ tests/core/test_arrow.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import pyarrow as pa
@@ -15,11 +16,18 @@ test_data = [
     {"col1": 5, "col2": "e"},
 ]
 
+# Config files to be referenced
+ABS_PATH = os.path.dirname(__file__)
+CFG_PATH = "files"
+TESTFILE = os.path.join(ABS_PATH, CFG_PATH, "test_arrow.csv")
+
 
 class ArrowConverterTest(unittest.TestCase):
-
     def setUp(self):
         self.arrow_converter = ArrowConverter()
 
     def test_convert_python_list_into_arrow(self):
         assert isinstance(self.arrow_converter.from_python_list(test_data), pa.Table)
+
+    def test_convert_csv_file_into_arrow(self):
+        assert isinstance(self.arrow_converter.from_csv(TESTFILE), pa.Table)
