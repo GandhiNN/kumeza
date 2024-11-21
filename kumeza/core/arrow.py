6c1eca137b6025377d9492ed858f78613191f9c9
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-21 10:44:11 +0700

    IIOT-14913: add csv writer

--- kumeza/core/arrow.py
+++ kumeza/core/arrow.py
@@ -2,7 +2,7 @@ import logging
 from typing import Any, Union
 
 import pyarrow as pa
-import pyarrow.csv
+import pyarrow.csv as csv
 import pyarrow.parquet as pq
 import s3fs
 
@@ -24,7 +24,7 @@ class ArrowConverter:
     @classmethod
     def from_csv(cls, f: str) -> pa.Table:
         logger.info("Converting CSV input to PyArrow table")
-        return pyarrow.csv.read_csv(f)
+        return csv.read_csv(f)
 
 
 class ArrowManager:  # pragma: no cover
@@ -77,6 +77,32 @@ class ArrowManager:  # pragma: no cover
                     root_path=path,
                     basename_template=obj_name,
                 )
+
+    @classmethod
+    def write_csv(
+        cls,
+        table: Union[pa.Table, list[pa.Table]],
+        path: str,  # path in local or can be S3 URI
+        table_name: str,
+        ingestion_flag: str,
+    ):
+        cur_date = dateobj.get_timestamp_as_str(ts_format="date_filename")
+        # input is a single pyarrow table object
+        if isinstance(table, pa.Table):
+            obj_name = f"{table_name}-00{{i}}-{cur_date}_utc_{ingestion_flag}.csv"
+            output_path = f"{path}/{obj_name}"
+            logger.info("Writing Arrow table to %s", output_path)
+            csv.write_csv(table, output_file=output_path)
+        # input is list of pyarrow tables
+        elif isinstance(table, list):  # noqa
+            for idx, t in enumerate(table):
+                seqnum = f"{idx:03}"  # 000, 001, 002...
+                obj_name = (
+                    f"{table_name}-{seqnum}-{cur_date}_utc_{ingestion_flag}.parquet"
+                )
+                output_path = f"{path}/{obj_name}"
+                logger.info("Writing Arrow table to %s", output_path)
+                csv.write_csv(t, output_file=output_path)
 
     @classmethod
     def read_from_s3(cls, s3uri: str) -> pq.ParquetDataset:
