b8d4da97e0ba968f6164890a3a5f8ccfa08b2ae8
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-29 06:12:35 +0700

    IIOT-14913: remove singleton decorator for now

--- kumeza/__init__.py
+++ kumeza/__init__.py
@@ -7,23 +7,12 @@ from logging import NullHandler
 logging.getLogger(__name__).addHandler(NullHandler())
 
 
-def singleton(cls):
-    instances = {}
-
-    def get_instance():
-        if cls not in instances:
-            instances[cls] = cls()
-        return instances[cls]
-
-    return get_instance()
-
-
-@singleton
 class Logger:
     def __init__(self, stream_type: str = "", log_level: str = "INFO"):
         self.logd = logging.getLogger(__name__)
         formatter = logging.Formatter(
-            "%(levelname)s : %(asctime)s - %(processName)s - %(threadName)s - %(module)s.%(funcName)s => %(message)s"
+            "%(levelname)s : %(asctime)s - %(processName)s - %(threadName)s "
+            "- %(module)s.%(funcName)s => %(message)s"
         )
         stream = io.StringIO() if stream_type == "string" else sys.stdout
         handler = logging.StreamHandler(stream)
