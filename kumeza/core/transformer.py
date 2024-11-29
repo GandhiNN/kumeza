e115cc276b99ecc934a5896a0ab7c4628d9675d0
Ngakan Gandhi <ngandhi@pmintl.net>
2024-11-30 06:05:59 +0700

    IIOT-14913: add unit test for transformer module

--- kumeza/core/transformer.py
+++ kumeza/core/transformer.py
@@ -6,26 +6,30 @@ def get_schema_hash(schema: list[dict]) -> str:
     return hashlib.sha256(json.dumps(schema, indent=4).encode("utf-8")).hexdigest()
 
 
-class SparkToHiveMapping:
+class SparkToHiveMapping:  # pragma: no-cover
+    dtype_mapping = {
+        "StringType": "string",
+        "IntegerType": "int",
+        "integer": "int",
+        "TimestampType": "string",
+        "timestamp": "string",
+        "BooleanType": "string",
+        "LongType": "bigint",
+        "long": "bigint",
+        "DoubleType": "double",
+        "BinaryType": "string",
+        "DateType": "string",
+        "DecimalType": "double",
+    }
 
-    def __init__(self):
-        self.dtype_mapping = {
-            "StringType": "string",
-            "IntegerType": "int",
-            "integer": "int",
-            "TimestampType": "string",
-            "timestamp": "string",
-            "BooleanType": "string",
-            "LongType": "bigint",
-            "long": "bigint",
-            "DoubleType": "double",
-            "BinaryType": "string",
-            "DateType": "string",
-            "DecimalType": "double",
-        }
-
-    def transform_schema(self, spark_dtype: str):
-        pass
+    @classmethod
+    def transform_schema(cls, spark_dtype: str) -> str:
+        try:
+            if "decimal" in str(spark_dtype):
+                return "float"
+            return cls.dtype_mapping[spark_dtype]
+        except Exception as e:
+            raise ValueError(f"{spark_dtype} is not implemented yet") from e
 
 
 class ArrowToHiveMapping:
@@ -38,7 +42,7 @@ class ArrowToHiveMapping:
     }
 
     @classmethod
-    def transform_schema(cls, arrow_dtype: str):
+    def transform_schema(cls, arrow_dtype: str) -> str:
         try:
             if "decimal" in str(arrow_dtype):
                 return "float"
--- tests/core/test_transformer.py
+++ tests/core/test_transformer.py
@@ -1,4 +1,108 @@
+import string
 import unittest
 
+# raise unittest.SkipTest("##TODO")
+import pytest
 
-raise unittest.SkipTest("##TODO")
+from kumeza.core.transformer import (
+    ArrowToHiveMapping,
+    SparkToHiveMapping,
+    get_schema_hash,
+)
+
+
+test_schema = [
+    {"name": "id", "type": "int", "description": "id"},
+    {"name": "item", "type": "string", "description": "item name"},
+    {
+        "name": "price",
+        "type": "float",
+        "description": "item price",
+    },
+]
+
+
+class TestTransformerUtils(unittest.TestCase):
+    def setUp(self):
+        self.hexstr = get_schema_hash(test_schema)
+
+    def test_get_schema_hash(self):
+        # assert if the resultant is a string type
+        assert isinstance(self.hexstr, str)
+
+        # assert if the resultant contains only hex digits (0...9, A...F)
+        hex_digits_set = set(string.hexdigits)
+        # if self.hexdigits is long, it's faster to chec against a set
+        assert all(c in hex_digits_set for c in self.hexstr) is True
+
+
+class TestSparkToHiveTransformer(unittest.TestCase):
+    def setUp(self):
+        self.mapping = SparkToHiveMapping()
+
+    def test_transform_stringtype_to_string(self):
+        assert self.mapping.transform_schema(spark_dtype="StringType") == "string"
+
+    def test_transform_integertype_to_string(self):
+        assert self.mapping.transform_schema(spark_dtype="IntegerType") == "int"
+
+    def test_transform_integer_to_int(self):
+        assert self.mapping.transform_schema(spark_dtype="integer") == "int"
+
+    def test_transform_timestamptype_to_string(self):
+        assert self.mapping.transform_schema(spark_dtype="TimestampType") == "string"
+
+    def test_transform_timestamp_to_string(self):
+        assert self.mapping.transform_schema(spark_dtype="timestamp") == "string"
+
+    def test_transform_booleantype_to_string(self):
+        assert self.mapping.transform_schema(spark_dtype="BooleanType") == "string"
+
+    def test_transform_longtype_to_bigint(self):
+        assert self.mapping.transform_schema(spark_dtype="LongType") == "bigint"
+
+    def test_transform_long_to_bigint(self):
+        assert self.mapping.transform_schema(spark_dtype="long") == "bigint"
+
+    def test_transform_doubletype_to_double(self):
+        assert self.mapping.transform_schema(spark_dtype="DoubleType") == "double"
+
+    def test_transform_binarytype_to_double(self):
+        assert self.mapping.transform_schema(spark_dtype="BinaryType") == "string"
+
+    def test_transform_datetype_to_string(self):
+        assert self.mapping.transform_schema(spark_dtype="DateType") == "string"
+
+    def test_transform_decimaltype_to_double(self):
+        assert self.mapping.transform_schema(spark_dtype="DecimalType") == "double"
+
+    def test_unimplemented_spark_type(self):
+        with pytest.raises(ValueError):
+            self.mapping.transform_schema(spark_dtype="blob")
+
+
+class TestArrowToHiveTransformer(unittest.TestCase):
+    def setUp(self):
+        self.mapping = ArrowToHiveMapping()
+
+    def test_transform_int64_to_int(self):
+        assert self.mapping.transform_schema(arrow_dtype="int64") == "int"
+
+    def test_transform_timestamp_to_string(self):
+        assert self.mapping.transform_schema(arrow_dtype="timestamp[us]") == "string"
+
+    def test_transform_string(self):
+        assert self.mapping.transform_schema(arrow_dtype="string") == "string"
+
+    def test_transform_bool_to_string(self):
+        assert self.mapping.transform_schema(arrow_dtype="bool") == "string"
+
+    def test_transform_null_to_string(self):
+        assert self.mapping.transform_schema(arrow_dtype="null") == "string"
+
+    def test_transform_decimal_to_float(self):
+        assert self.mapping.transform_schema(arrow_dtype="decimal") == "float"
+
+    def test_unimplemented_arrow_type(self):
+        with pytest.raises(ValueError):
+            self.mapping.transform_schema(arrow_dtype="blob")
